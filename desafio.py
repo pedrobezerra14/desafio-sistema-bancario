31f4dbfd9c420d2c17bf62b76b67198c51a5b28e
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-05-06 08:40:20 -0300

    desafio finalizado

--- desafio.py
+++ desafio.py
@@ -21,19 +21,30 @@ while True:
         valor_deposito = float(input('Informe o valor que deseja depositar: '))
         if valor_deposito > 0 and valor_deposito <= 500:
             saldo += valor_deposito
-            extrato += (f'R${valor_deposito:.2f}')
+            extrato += (f'Depósito: R${valor_deposito:.2f}')
         else:
             print('Só é possível depositar valores positivos!')
 
-    # elif opcao == "s":
+    elif opcao == "s":
+        if numero_saques <= LIMITE_SAQUES:
+            valor_saque = float(input('Informe o valor que deseja sacar: '))
+            if valor_saque <= 500:
+                numero_saques += 1
+                saldo -= valor_saque
+                extrato += (f'Saque: R${valor_saque:.2f}')
+            else:
+                print('Você só pode sacar até R$500!')
+        else:
+            print('Você excedeu o limite de saques!')
+                
         
 
     elif opcao == "e":
-        print(f'''
-            ================ EXTRATO ================
-                            {extrato}
-            ==========================================
-        ''')
+        
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
 
     elif opcao == "q":
         break
