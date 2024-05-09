7b3cfac1662e4947b141202348c5f5677ab0807f
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-05-09 10:31:24 -0300

    função sacar

--- otimização/desafio_otimizado.py
+++ otimização/desafio_otimizado.py
@@ -13,11 +13,39 @@ def menu():
     => """
     return input(textwrap.dedent(menu))
 
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    
+    valor = float(input("Informe o valor do saque: "))
+
+    excedeu_saldo = valor > saldo
+
+    excedeu_limite = valor > limite
+
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    
+
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
-LIMITE_SAQUES = 3
+limite_saques = 3
 
 while True:
 
@@ -40,7 +68,7 @@ while True:
 
         excedeu_limite = valor > limite
 
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        excedeu_saques = numero_saques >= limite_saques
 
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
