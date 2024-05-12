6b468ee60b3318503529a7c6ebd4e55454572820
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-05-12 12:03:29 -0300

    ultima parte do desafio

--- otimização/desafio_otimizado.py
+++ otimização/desafio_otimizado.py
@@ -54,6 +54,45 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
     print("==========================================")
     
+def criar_usuario(usuarios):
+    cpf = input('Informe o CPF (apenas números): ')
+    usuario = filtrar_usuario(cpf,usuarios)
+    
+    if usuario:
+        print('\n@@@ Já existe usuário com esse CPF! @@@')
+        return
+    
+    nome = input('Informe o nome completo: ')
+    data_nascimento = input('Informe a data de nascimento (dd-mm-aaaa): ')
+    endereco = input('Informe o endereço (logradouro, número - bairro - cidade/sigla do estado): ')
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+    
+    print("=== Usuário criado com sucesso! ===")
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input('Informe o CPF do usuário: ')
+    usuario = filtrar_usuario(cpf, usuarios)
+    
+    if usuario:
+        print('\n=== Conta criada com sucesso! ===')
+        return{"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+    
+    print('\n@@@Usuário não encontrado, fluxo de criação de conta encerrado! @@@')
+    
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
 def main():
     LIMITE_SAQUES = 3
     AGENCIA = "0001"
@@ -86,11 +125,24 @@ def main():
 
         elif opcao == "e":
             exibir_extrato(saldo, extrato=extrato)
+            
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+        
+        elif opcao == "nc":
+            numero_conta  = len(contas)  + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+            
+            if conta:
+                contas.append(conta)
+                
+        elif opcao == "lc":
+            listar_contas(contas)
 
         elif opcao == "q":
             break
 
         else:
-            print("Operação inválida, por favor selecione novamente a operação desejada.")
+            print("\n@@@Operação inválida, por favor selecione novamente a operação desejada. @@@")
         
 main()
